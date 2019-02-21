9b0267910e3759a66154c2d472b8afe456e72597
Samuel Theodore <47284495+samueltheodore@users.noreply.github.com>
2019-02-21 09:25:37 -0800

    Update PPG 2/21/2019
    added comments

--- ppg.py
+++ ppg.py
@@ -1,6 +1,7 @@
 import random, time
 from random import randint
 
+# what the characters of the passwords being used 
 everything = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@£$%^&*().,?0123456789'
 loweronly = 'abcdefghijklmnopqrstuvwxyz'
 upperonly = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
@@ -12,6 +13,7 @@ numbersonly = '0123456789'
 secure = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ!@£$%^&*().,?0123456789'
 utf8 = '!"#$%&*+,-./0123456789:;<=>? ABCDEFGHIJKLMNOPQRSTUVWXYZ[\]^_`abcdefghijklmnopqrstuvwxyz{|}~€‚ƒ„…†‡ˆ‰Š‹ŒŽ‘’“”•–—˜™š›œžŸ ¡¢£¤¥¦§¨©ª«¬ ®¯°±²³´µ¶·¸¹º»¼½¾¿ÀÁÂÃÄÅÆÇÈÉÊËÌÍÎÏÐÑÒÓÔÕÖ×ØÙÚÛÜÝÞßàáâãäåæçèéêëìíîïðñòóôõö÷øùúûüýþÿĀāĂăĄąĆćĈĉĊċČčĎďĐđĒēĔĕĖėĘęĚěĜĝĞğĠġĢģĤĥĦħĨĩĪīĬĭĮįİıĲĳĴĵĶķĸĹĺĻļĽľĿŀŁłŃńŅņŇňŉŊŋŌōŎŏŐőŒœŔŕŖŗŘřŚśŜŝŞş'
 
+# displays this to terminal
 print('''
 
      /$$$$$$$  /$$$$$$$   /$$$$$$                       /$$$$$$                                              /$$
@@ -21,7 +23,7 @@ print('''
     | $$____/ | $$____/ | $$|_  $$      |______/       \____  $$  /$$$$$$$| $$ \ $$ \ $$| $$  | $$| $$$$$$$$| $$
     | $$      | $$      | $$  \ $$                     /$$  \ $$ /$$__  $$| $$ | $$ | $$| $$  | $$| $$_____/| $$
     | $$      | $$      |  $$$$$$/                    |  $$$$$$/|  $$$$$$$| $$ | $$ | $$|  $$$$$$/|  $$$$$$$| $$
-    |__/      |__/       \______/                      \______/  \_______/|__/ |__/ |__/ \______/  \_______/|__/ 
+    |__/      |__/       \______/                      \______/  \_______/|__/ |__/ |__/ \______/  \_______/|__/
                                                                                                                 
           +---------------------------------Powerful Password Generator-----------------------------------+                                                                                                       
                                                                                                             
@@ -34,37 +36,38 @@ print('''
         [7]  only lower with numbers
         [8]  numbers only
         [9]  secure
-        [10] secure+ (utf-8)
+        [10] secure+ (utf-8, unstable on windows)
 
-          +---------------------------------Powerful Password Generator-----------------------------------+             
+          +---------------------------------Powerful Password Generator-----------------------------------+    
+          // disclaimer: this script does not remeber passwords for you!         
  
 ''')
 
-choice = input("choose a type of password from above: ")
+choice = input("choose a type of password from above: ") # asks what type of password user wants
 
-if choice == "1":
-    number = 1
+if choice == "1": # if choise is value; do the following.
+    number = 1 # not a necessity but useful
 
-    length = input('number of characters: ')
-    length = int(length)
-    if length < 8:
-        print("Warning: a password with 8 charaters or less is not reccomended!")
-        time.sleep(1) 
-    else:  
-        print("please wait..")
-        time.sleep(0.3)
+    length = input('number of characters: ') # asks user how many charaters he/she wants in there password.
+    length = int(length) # tels the code to make the password the amount of charaters the user said.
+    if length < 8: # if the user choose to have less than 8 characters, a waring will apper. 
+        print("Warning: a password with 8 charaters or less is not reccomended!") # makes the warning display
+        time.sleep(1) # a timer so the user reads the the warning. 
+    else: # if the users chooses to have more than 8 characters the error won't apper.
+        print("please wait..") # makes the program more natural.
+        time.sleep(0.3) # wait 0.3 secconds.
 
-    for pwd in range(number):
-      password = ''
-    for c in range(length):
-        password += random.choice(loweronly)
-    print("+--------------------------------------+")
+    for pwd in range(number): # how many passwords to make, this is disabeld because number = 1.
+      password = '' 
+    for c in range(length): # makes the password length the length the user said.
+        password += random.choice(loweronly) # makes the password, what type of password.
+    print("+--------------------------------------+") # to help user see their password
     print('Your password: ' +password)
     print("+--------------------------------------+")
-    print("Thanks for using my password generator!")
+    print("Thanks for using my password generator!") # a thank you note <3
     input()
 
-if choice == "2":
+if choice == "2": # if choise is value; do the following.
     number = 1
 
     length = input('number of characters: ')
@@ -86,7 +89,7 @@ if choice == "2":
     print("Thanks for using my password generator!")
     input()
 
-if choice == "3":
+if choice == "3": # if choise is value; do the following.
     number = 1
 
     length = input('number of characters: ')
@@ -108,7 +111,7 @@ if choice == "3":
     print("Thanks for using my password generator!")
     input()
 
-if choice == "4":
+if choice == "4": # if choise is value; do the following.
     number = 1
 
     length = input('number of characters: ')
@@ -130,7 +133,7 @@ if choice == "4":
     print("Thanks for using my password generator!")
     input()
 
-if choice == "5":
+if choice == "5": # if choise is value; do the following.
     number = 1
 
     length = input('number of characters: ')
@@ -152,7 +155,7 @@ if choice == "5":
     print("Thanks for using my password generator!")
     input()
 
-if choice == "6":
+if choice == "6": # if choise is value; do the following.
     number = 1
 
     length = input('number of characters: ')
@@ -174,7 +177,7 @@ if choice == "6":
     print("Thanks for using my password generator!")
     input()
 
-if choice == "7":
+if choice == "7": # if choise is value; do the following.
     number = 1
 
     length = input('number of characters: ')
@@ -196,7 +199,7 @@ if choice == "7":
     print("Thanks for using my password generator!")
     input()
 
-if choice == "8":
+if choice == "8": # if choise is value; do the following.
     number = 1
 
     length = input('number of characters: ')
@@ -218,7 +221,7 @@ if choice == "8":
     print("Thanks for using my password generator!")
     input()
 
-if choice == "9":
+if choice == "9": # if choise is value; do the following.
 
     number = 1
     
@@ -235,7 +238,7 @@ if choice == "9":
     print("Thanks for using my password generator!")
     input()
 
-if choice == "10":    
+if choice == "10": # if choise is value; do the following.
     length = (randint(20, 99))
 
     number = 1
@@ -248,5 +251,6 @@ if choice == "10":
     print('Your password: ' +password)
     print("+--------------------------------------+")
     print("password length : %d" % len (password)) 
-    print("Thanks for using my password generator!")
-    input()
+    print("Thanks for using my password generator! you have 20 secconds to copy password.")
+    time.sleep(20)
+    exit()
